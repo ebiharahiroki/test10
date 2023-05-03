d278568a8e5192067ff58f55c1091337f0ff9511
OOTSUJIYUUSUKE <tsujichan221@gmail.com>
2023-05-04 01:01:16 +0900

    CRUD処理のReadのみ実装

--- src/main/java/com/example/MyBatisDemo/mapper/Anime_moviesMapper.java
+++ src/main/java/com/example/MyBatisDemo/mapper/Anime_moviesMapper.java
@@ -9,7 +9,7 @@ import java.util.List;
 @Mapper
 public interface Anime_moviesMapper {
     @Select("SELECT * FROM anime_movies")
-    List<Anime_movies> findAll();git inti
-    @Select("SELECT * FROM anime_movies WHERE id = #{id}")
-    Anime_movies findById(int id);
+    List<Anime_movies> findAll();
+    @Select("SELECT * FROM anime_movies WHERE published_year = #{published_year}")
+    List<Anime_movies> findByPublishedYear(String published_year);
 }
