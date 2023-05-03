0ca8da04ea3ee8385801655d383f63c26cc7a5f5
OOTSUJIYUUSUKE <tsujichan221@gmail.com>
2023-05-04 01:02:54 +0900

    CRUD処理のReadのみ実装

--- src/main/java/com/example/MyBatisDemo/controller/Anime_moviesController.java
+++ src/main/java/com/example/MyBatisDemo/controller/Anime_moviesController.java
@@ -3,6 +3,7 @@ package com.example.MyBatisDemo.controller;
 import com.example.MyBatisDemo.entity.Anime_movies;
 import com.example.MyBatisDemo.service.Anime_moviesService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,12 +16,19 @@ public class Anime_moviesController {
         this.anime_moviesService = anime_moviesService;
     }
     @GetMapping("/anime_movies")
-    public List<Anime_moviesResponse> anime_movies() {
-        List<Anime_movies> anime_movies = anime_moviesService.findAll();
+    public List<Anime_moviesResponse> anime_moviesAll() {
+        List<Anime_movies> anime_moviesAll = anime_moviesService.findAll();
+        List<Anime_moviesResponse> anime_MoviesAllResponse = anime_moviesAll.stream()
+                .map(Anime_moviesResponse::new)
+                .toList();
+        return anime_MoviesAllResponse;
+    }
+    @GetMapping("/anime_movies/{published_year}")
+    public List<Anime_moviesResponse> anime_movies(@PathVariable("published_year") String published_year) {
+        List<Anime_movies> anime_movies = anime_moviesService.findByPublishedYear(published_year);
         List<Anime_moviesResponse> anime_MoviesResponse = anime_movies.stream()
                                                         .map(Anime_moviesResponse::new)
                                                         .toList();
         return anime_MoviesResponse;
     }
-
 }
--- src/main/java/com/example/MyBatisDemo/service/Anime_moviesServiceImpl.java
+++ src/main/java/com/example/MyBatisDemo/service/Anime_moviesServiceImpl.java
@@ -5,11 +5,10 @@ import com.example.MyBatisDemo.mapper.Anime_moviesMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class Anime_moviesServiceImpl implements Anime_moviesService {
-    private Anime_moviesMapper anime_moviesMapper;
+    final private Anime_moviesMapper anime_moviesMapper;
     public Anime_moviesServiceImpl(Anime_moviesMapper anime_moviesMapper) {
         this.anime_moviesMapper = anime_moviesMapper;
     }
@@ -18,16 +17,7 @@ public class Anime_moviesServiceImpl implements Anime_moviesService {
         return anime_moviesMapper.findAll();
     }
     @Override
-    public Anime_movies findById(int id) {
-        return anime_moviesMapper.findById(id);
+    public List<Anime_movies> findByPublishedYear(String published_year) {
+        return anime_moviesMapper.findByPublishedYear(published_year);
     }
-    @Override
-    public void createAnime_movies(int id, String title, String published_year) {
-
-    }
-    @Override
-    public void updateAnime_movies(int id, String title, String published_year) {
-
-    }
-
 }
